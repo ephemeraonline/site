0e76d6cc9e4f5183d0cbb815ac3b7898bdd7d553
ephemeraonline <amad@ephemeraonline.world>
2026-04-01 13:40:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -230,14 +230,29 @@ let isPlaying = false;
 const curr_track = document.getElementById("music");
 
 const track_list = [
+const track_list = [
   { name: "HYD - Angel", path: "https://files.catbox.moe/sre606.mp3" },
   { name: "2Charm - prerogative", path: "https://files.catbox.moe/srlpzf.mp3" },
   { name: "Charli xcx - Sympathy is a knife featuring ariana grande", path: "https://files.catbox.moe/xlx839.mp3" },
   { name: "Mckayla Twiggs - What A Girl Wants", path: "https://files.catbox.moe/xw4kit.mp3" },
   { name: "Danny L Harle - Raft in the Sea Featuring Julia Michaels", path: "https://files.catbox.moe/7ehw00.mp3" },
-  { name: "Metric - Time is a Bomb", path: "https://files.catbox.moe/txd5we.mp3" }
-];
+  { name: "Metric - Time is a Bomb", path: "https://files.catbox.moe/txd5we.mp3" },
 
+  { name: "Placeholder Track 07", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 08", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 09", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 10", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 11", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 12", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 13", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 14", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 15", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 16", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 17", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 18", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 19", path: "https://files.catbox.moe/sre606.mp3" },
+  { name: "Placeholder Track 20", path: "https://files.catbox.moe/sre606.mp3" }
+];
 function syncUI() {
   if (mobile_track_name) {
     mobile_track_name.textContent = track_list[track_index].name;
